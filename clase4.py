36c72950faafc34d50edd9b09198ec05daf26924
Santiago Millan <santiagomillanpardo@gmail.com>
2025-06-29 23:06:31 -0500

    Clase 4

---
+++ clase4.py
@@ -0,0 +1,35 @@
+name = "Danna"
+# se pueden usar "" '' # o """ """ para definir cadenas de texto 
+print(name)
+print("-----")
+print(name[0])  # Imprime la primera letra de la cadena
+print(name[1])  # Imprime la segunda letra de la cadena 
+print("-----")
+print(name[-3])
+print(name[-4])
+
+last_name = "Veterinaria"
+print(last_name)
+print("-----")
+print(name + " " + last_name)  # Concatenación de cadenas
+print("-----")
+print(5 * name)  # Repite la cadena 3 veces
+print("-----")
+print(len(name))
+print("-----")
+print(name.lower()) 
+print(name.upper())  # Convierte la cadena a mayúsculas
+
+# .count() 
+# .capitalize()
+# .title() 
+# .swapcase() 
+# .replace(,) 
+# .split() 
+# .strip() 
+# .lstrip() 
+# .rstrip() 
+# .find()
+# .index() 
+# eval()	#Este y el siguiente son super métodos
+# exec()
