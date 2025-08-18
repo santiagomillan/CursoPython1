b40293d803db364e28b5e55fb4920b7e4fc601f6
Santiago Millan <santiagomillanpardo@gmail.com>
2025-08-18 01:51:42 -0500

    Clase 16/63

---
+++ clase-13.py
@@ -0,0 +1,26 @@
+numbers = [1, 2, 3, 4, 5, 6]
+for i in numbers:
+    print("Aqui es igual a:",i+1)
+    # print("Aqui es igual a:",i)
+
+for i in range(3,10):
+    print(i)
+
+fruits = ["Manzana", "Pera", "Uva", "Naranja", "Tomate"]
+for fruit in fruits:
+    print(fruit)
+    if fruit == "Naranja":
+        print("Naranja encontrada")
+
+x = 0
+while x<5:
+    if x ==3:
+        break
+    print(x) 
+    x +=1
+
+numbers = [1, 2, 3, 4, 5, 6]
+for i in numbers:
+    if i ==3:
+        break
+    print("Aqui es igual a:",i)
