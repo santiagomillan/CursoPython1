1890a75d6e2de074829619246a6023f6ffae91e7
Santiago Millan <santiagomillanpardo@gmail.com>
2025-08-31 01:35:16 -0500

    Clase 22/63 p1

---
+++ clase23.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print( f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+person1 = Person("Alice", 30)
+person2 = Person("Bob", 25)
+person1.greet()
+person2.greet()
