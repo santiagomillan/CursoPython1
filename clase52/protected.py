f95e7b94f4010e345c5079361ef77923d8edef88
Santiago Millan <santiagomillanpardo@gmail.com>
2025-09-28 13:22:50 -0500

    clase 52/63

---
+++ clase52/protected.py
@@ -0,0 +1,21 @@
+class BaseClass:
+    def __init__(self):
+        self._protected_variable = 'Protected'
+        self.__private_variable = 'Private'
+
+    def _protected_method(self):
+        print('This is a protected method')
+    
+    def __private_method(self):
+        print('This is a private method')
+    
+    def public_method(self):
+        self.__private_method()
+
+    
+    
+base = BaseClass()
+# print(base._protected_variable)  # Accessing protected variable
+# print(base._protected_method())   # Accessing protected method
+# base._protected_method()
+print(base.__private_variable)  # Accessing private method through a public method
